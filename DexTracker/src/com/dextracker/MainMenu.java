e00b75e2ac4c8aa44a505aee0f3c9c4fd35fc8c2
Markearlie <mark.earlie@gmail.com>
2014-11-26 01:12:46 +0000

    Added main menu navigation

---
+++ DexTracker/src/com/dextracker/MainMenu.java
@@ -0,0 +1,63 @@
+package com.dextracker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainMenu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+	}
+	
+	 public void btnGameOne_onClick(View v)
+	    {
+	    	Intent intent = new Intent(this, GameOne.class);
+	    	startActivity(intent);
+	    }
+//	    public void btnGameTwo_onClick(View v)
+//	    {
+//	    	Intent intent = new Intent(this, GameTwo.class);
+//	    	startActivity(intent);
+//	    }
+//	    public void btnGameThree_onClick(View v)
+//	    {
+//	    	Intent intent = new Intent(this, GameThree.class);
+//	    	startActivity(intent);
+//	    }
+	    public void btnLeaderboard_onClick(View v)
+	    {
+	    	Intent intent = new Intent(this, Leaderboard.class);
+	    	startActivity(intent);
+	    }
+//	    public void btnSettings_onClick(View v)
+//	    {
+//	    	Intent intent = new Intent(this, Settings.class);
+//	    	startActivity(intent);
+//	    }
+//	    
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
