ddbdaaa0956ab7ebb4e776cc344efbefdf0b5ffb
Markearlie <mark.earlie@gmail.com>
2014-11-27 02:31:17 +0000

    Customizable submit popup, alias creation through popup:

--- DexTracker/src/com/dextracker/SubmitScoreDialogFragment.java
+++ DexTracker/src/com/dextracker/SubmitScoreDialogFragment.java
@@ -5,19 +5,34 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+
 import net.epsilonlabs.datamanagementefficient.library.DataManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class SubmitScoreDialogFragment extends DialogFragment {
 
 	Context context;
 	private int score, miss;
+	EditText input;
+
+	Button btnAgain, btnSubmit;
+	TextView tv1 , tv2;
+	
 	Calendar cal = new GregorianCalendar();
 
 	public int getScore() {
@@ -38,55 +53,74 @@ public class SubmitScoreDialogFragment extends DialogFragment {
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		// Use the Builder class for convenient dialog construction
-		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		builder.setMessage("Score = "  + getScore() + "\n" + "You missed: " + getMiss())
+//		// Use the Builder class for convenient dialog construction
+//		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//		
+//		input = new EditText(getActivity());
+//		input.setHint("Name");
+//		
+//		builder.setTitle("Submit Score?")
+//		.setView(input)
+//		.setMessage("Hit: "  + getScore() + "Missed: " + getMiss() )
+//		.setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
+//			public void onClick(DialogInterface dialog, int id) {
+//				try{
+//					
+//					String alias = input.getText().toString();
+//					if(!alias.isEmpty())
+//					{
+//					//Add this code to database layer
+//					DexTrackerDAO dao = new DexTrackerDAO(context);
+//					int pID, sID;
+//					
+//					
+//					
+//					Player p = new Player(alias);
+//					pID = dao.storePlayer(p);
+//
+//					Score s = new Score(getScore(), getMiss());
+//					sID = dao.storeScore(s);
+//
+//					Game g = new Game(pID, sID, "Sequential");
+//					dao.storeGame(g);
+//					
+//					}
+//
+//				}catch(Exception ex){
+//					ex.printStackTrace();
+//				}
+//			}
+//		})
+//		.setNegativeButton(R.string.play_again, new DialogInterface.OnClickListener() {
+//			public void onClick(DialogInterface dialog, int id) {
+//
+//				//Play again code here, perhaps relaunch activity - see overhead/time required.
+//
+//			}
+//		});
+//		// Create the AlertDialog object and return it
+//
+//		return builder.create();
+		
+		
+		final Dialog dialog = new Dialog(getActivity());  
+		  dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);  
+		  dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,  
+		    WindowManager.LayoutParams.FLAG_FULLSCREEN);  
+		  dialog.setContentView(R.layout.submit_dialog);
+		  dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
-		.setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int id) {
-
-				//                		List<Player> listOfPlayers = new ArrayList<Player>();
-				//                		dm = DataManager.getInstance(this);
-				//                		dm.open();
-				//
-				//                		listOfPlayers = dm.getAll(Player.class);
-				//                		for(Player p: listOfPlayers)
-				//                		{
-				//                			Log.i("Player+" ,p.toString());
-				//                		}
-
-				try{
-					//Add this code to database layer
-					DexTrackerDAO dao = new DexTrackerDAO(context);
-					int pID, sID;
-
-					Player p = new Player("Mark");
-					pID = dao.storePlayer(p);
-
-					Score s = new Score(getScore(), getMiss());
-					sID = dao.storeScore(s);
-
-
-					Game g = new Game(pID, sID, "Sequential");
-					dao.storeGame(g);
-					
-
-
-				}catch(Exception ex){
-					ex.printStackTrace();
-				}
-			}
-		})
-		.setNegativeButton(R.string.play_again, new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int id) {
-
-				//Play again code here, perhaps relaunch activity - see overhead/time required.
-
-			}
-		});
-		// Create the AlertDialog object and return it
-
-		return builder.create();
+		  dialog.show();  
+		  btnAgain = (Button) dialog.findViewById(R.id.button1);  
+		  tv1 = (TextView) dialog.findViewById(R.id.textView1);  
+		  btnAgain.setOnClickListener(new OnClickListener() { 
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			dismiss();  
+		}  
+		  });  
+		  return dialog;  
 	}
 
 	public void setContext(Context context) {
